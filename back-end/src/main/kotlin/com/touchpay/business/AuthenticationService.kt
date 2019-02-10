26a3e72e6b54d2aa7ec5cf44f1812c22e01cf674
Bruno <brmcerqueira@gmail.com>
2019-02-10 05:11:21 -0200

    Criação de rota de registro

--- back-end/src/main/kotlin/com/touchpay/business/AuthenticationService.kt
+++ back-end/src/main/kotlin/com/touchpay/business/AuthenticationService.kt
@@ -2,14 +2,23 @@ package com.touchpay.business
 
 import com.touchpay.business.validators.ValidatorBuilder
 import com.touchpay.business.validators.notEmpty
+import com.touchpay.common.Auditor
 import com.touchpay.dto.SignInDto
 import com.touchpay.exceptions.SignInException
 import com.touchpay.persistence.dao.AuthenticationDao
+import com.touchpay.persistence.json.JsonBuilder
 import io.reactivex.Single
+import io.vertx.reactivex.ext.auth.jwt.JWTAuth
 import org.mindrot.jbcrypt.BCrypt
+import java.time.LocalDateTime
+import java.time.ZoneId
 import javax.inject.Inject
+import javax.inject.Named
 
-class AuthenticationService @Inject constructor(validatorBuilder: ValidatorBuilder, private val dao: AuthenticationDao) {
+class AuthenticationService @Inject constructor(validatorBuilder: ValidatorBuilder,
+                                                private val auth: JWTAuth,
+                                                @Named("business") private val auditor: Auditor,
+                                                private val dao: AuthenticationDao) {
 
     private val validatorSignInDto = validatorBuilder.create<SignInDto> {
         let { email } check {
@@ -29,7 +38,24 @@ class AuthenticationService @Inject constructor(validatorBuilder: ValidatorBuild
                 throw SignInException()
             }
 
-            it.get()
+            val now = LocalDateTime.now()
+            val json = JsonBuilder {
+                //sub (subject) = Entidade à quem o token pertence, normalmente o ID do usuário;
+                "sub" to it.get()
+                //iss (issuer) = Emissor do token;
+                "iss" to "touchpay"
+                //iat (issued at) = Timestamp de quando o token foi criado;
+                "iat" to now.atZone(ZoneId.systemDefault()).toEpochSecond()
+                //exp (expiration) = Timestamp de quando o token irá expirar;
+                "exp" to now.plusDays(3).atZone(ZoneId.systemDefault()).toEpochSecond()
+                //aud (audience) = Destinatário do token, representa a aplicação que irá usá-lo.
+                "aud" to "front-end"
+                "permissions" to arrayOf<String>()
+            }.json
+
+            auditor.info("jwt-payload -> {0}", json)
+
+            auth.generateToken(json)
         }
     }
 }
