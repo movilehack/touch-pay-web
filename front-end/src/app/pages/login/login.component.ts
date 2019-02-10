b9d24c5d38f7f0f191f0ae436cd69cb7cbe44c0c
Lucas Simas <simas.lucas@hotmail.com>
2019-02-09 23:12:19 -0300

    Template tela de login finalizada

--- front-end/src/app/pages/login/login.component.ts
+++ front-end/src/app/pages/login/login.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,46 +10,37 @@ import { LoginService } from 'src/app/services/login.service';
   encapsulation: ViewEncapsulation.Emulated
 })
 export class LoginComponent implements OnInit {
-  
-  @ViewChild("modal")
-  private modal
 
-  private formGroup: FormGroup
+  private formGroup: FormGroup;
 
   constructor(private loginService: LoginService, private fb: FormBuilder, private router: Router) {
     this.formGroup = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(5)]],
-    })
-
+    });
   }
 
   ngOnInit() {
   }
 
-  login(){
-    this.lock()
+  login() {
+    this.lock();
     this.loginService.login(this.formGroup.controls['email'].value, this.formGroup.controls['password'].value).subscribe(
       response => {
-        console.log('response')
-        this.router.navigate([response.role.toLowerCase()])
-        this.unlock()
+        console.log('response');
+        this.router.navigate(['dashboard']);
+        this.unlock();
       },
-      error => {
-        this.modal.openModal()
-        this.unlock()
-      },
-    )
+    );
   }
 
-  private lock(){
-    this.formGroup.controls['email'].disable()
-    this.formGroup.controls['password'].disable()
+  private lock() {
+    this.formGroup.controls['email'].disable();
+    this.formGroup.controls['password'].disable();
   }
 
-  private unlock(){
-    this.formGroup.controls['email'].enable()
-    this.formGroup.controls['password'].enable()
+  private unlock() {
+    this.formGroup.controls['email'].enable();
+    this.formGroup.controls['password'].enable();
   }
-
 }
